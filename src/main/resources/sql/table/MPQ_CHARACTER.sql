eba343d088e7f78ecde3032f9b41614019819f03
Chris Spears <zometer@gmail.com>
2022-01-19 11:05:17 -0600

    changing the field names "name" and "subtitle" to "name_key" and "subtitle_key", since the plan is now to create a view that looks up by locale.

--- src/main/resources/sql/table/MPQ_CHARACTER.sql
+++ src/main/resources/sql/table/MPQ_CHARACTER.sql
@@ -1,16 +1,16 @@
-/* 
+/*
  * MPQ_CHARACTER - Character identity data.
  */
 
--- drop table mpq_data.mpq_character ; 
+-- drop table mpq_data.mpq_character ;
 
 create table mpq_data.mpq_character (
-	mpq_charcter_id varchar(100) primary key, 
-    name varchar(250) not null, 
-    subtitle varchar(250) not null, 
-    rarity integer not null, 
-    release_date timestamp
-); 
+	mpq_character_id varchar(100) primary key,
+	name_key varchar(250) not null,
+	subtitle_key varchar(250) not null,
+	rarity integer not null,
+	release_date timestamp
+);
 
 comment on table mpq_data.mpq_character is 'Character identity data';
 
