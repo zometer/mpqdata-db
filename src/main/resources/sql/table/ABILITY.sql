da767637314887bdb22d814272d9486957e82006
Chris Spears <zometer@gmail.com>
2022-01-19 11:06:53 -0600

    changing the "name/description" fields to "name_key/description_key" (for locale lookups in views), and adding the "ordinal position" field.

--- src/main/resources/sql/table/ABILITY.sql
+++ src/main/resources/sql/table/ABILITY.sql
@@ -1,16 +1,18 @@
-/* 
+/*
  * ABILITY - Character ability data.
  */
 
--- drop table mpq_data.ability ; 
+-- drop table mpq_data.ability ;
 
 create table mpq_data.ability (
-	ability_id varchar(100) primary key, 
-    name varchar(250) not null, 
-    description varchar(500) not null, 
-    color varchar(50) not null, 
-    cost integer
-); 
+	ability_id varchar(100) primary key,
+	mpq_character_id varchar(100) references mpq_data.mpq_character(mpq_character_id),
+	name_key varchar(250) not null,
+	description_key varchar(500) not null,
+	color varchar(50) not null,
+	ordinal_position int not null,
+	cost integer
+);
 
 comment on table mpq_data.ability is 'Core identity data for character abilities';
 
