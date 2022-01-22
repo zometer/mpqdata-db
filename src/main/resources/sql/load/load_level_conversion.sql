87b9557e43120e762741baa9875d558be4bb4073
Chris Spears <zometer@gmail.com>
2022-01-21 18:42:15 -0600

    Loading level conversion data.

---
+++ src/main/resources/sql/load/load_level_conversion.sql
@@ -0,0 +1,9 @@
+/*
+ * Load effective level conversion table.  
+ */ 
+
+copy mpq_data.level_conversion (rarity, effective_level, display_level)
+from '/docker-entrypoint-initdb.d/level_conversion-initial-load.csv'
+delimiter ','
+csv header
+;
