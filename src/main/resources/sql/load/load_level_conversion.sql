d62499cd7840abe2dee0b22832f73b5714d684d3
Chris Spears <zometer@gmail.com>
2022-01-24 12:39:45 -0600

    1: DB: Deploy Database to EKS
    Task-Url: https://github.com/zometer/mpqdata-db/issues/1
    
    whitespace

--- src/main/resources/sql/load/load_level_conversion.sql
+++ src/main/resources/sql/load/load_level_conversion.sql
@@ -1,6 +1,6 @@
 /*
- * Load effective level conversion table.  
- */ 
+ * Load effective level conversion table.
+ */
 
 copy mpq_data.level_conversion (rarity, effective_level, display_level)
 from '/docker-entrypoint-initdb.d/level_conversion-initial-load.csv'
